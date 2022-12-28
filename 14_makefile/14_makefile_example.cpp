3d4dcd286e91dbea4b84af76b6283e3f58e0d950
ITWorks4U <ITWorks4U@gmail.com>
2022-12-28 19:32:04 +0100

    basic makefile usage

---
+++ 14_makefile/14_makefile_example.cpp
@@ -0,0 +1,20 @@
+/*
+	This source file doesn't contain a complex structure.
+	The focus lies on the makefile itself.
+
+	The given makefiles are defined for Linux only.
+	Make sure the application 'make' is installed on your system.
+
+	on command line: type 'whereis make' to look for make
+	'sudo apt install make' <-> install make on your system
+
+	for Windows:	Use mingw for your process.
+	video link: https://www.youtube.com/watch?v=44OBLzkkA0I&list=PLwMeVY6dlAg20kre011gbHHzMl77SIi-g&index=17
+*/
+
+#include <iostream>
+
+int main() {
+	/*	nothing to do here	*/
+	return 0;
+}
