53645fca3c508d522332f4dafd001fe3928216af
ITWorks4U <itworks4U@gmail.com>
2024-05-24 17:12:20 +0200

    pitfalls with friend keyword

---
+++ 40_friend_pitfalls/40_friend_pitfalls_1.cpp
@@ -0,0 +1,92 @@
+/*
+	In C++ you can declare a class to be a "friend" for an another class to allow
+	to gain access to protected and private members. Basicly in OOP it makes no sense
+	to allow a security flaw, however, you'll learn how to use friend in C++.
+*/
+#include <iostream>
+using namespace std;
+
+class BaseClass {
+	int private_member;
+	void unable_to_display() {
+		cout << "the secret password is: ..." << endl;
+	}
+
+	protected:
+		double protected_member;
+	
+	public:
+		BaseClass() {}
+		virtual ~BaseClass() {}
+
+		/*	...	*/
+
+		//	declaration to be a "friend" for another classes
+		//	this allows all given classes to gain access to EVERYTHING
+		friend class ForeignClass;
+};
+
+/*	somewhere in your project...	*/
+
+/*
+	This class has the granted access to everything for BaseClass,
+	because it's a friend of BaseClass.
+*/
+class ForeignClass {
+	public:
+		void access() {
+			BaseClass bc;
+
+			/*	you're also able to modify the members, which shouldn't be able...	*/
+			bc.private_member = (0x100 >> 8) & 0x1;
+			bc.protected_member = 123.456;
+
+			cout	<< "access #1:" << endl
+					<< "let's see the private member: " << bc.private_member << endl
+					<< "What about the protected member? " << bc.protected_member << endl;
+		}
+
+		void access(BaseClass &bc) {
+			/*
+				with the argument you're also able to do the same thing;
+				by using const this allows you only to read the members
+			*/
+
+			cout	<< "access #2:"
+					<< endl << "let's see the private member: " << bc.private_member << endl
+					<< "What about the protected member? " << bc.protected_member << endl;
+
+			bc.unable_to_display();
+		}
+
+		/*
+			Fun fact: Since access comes with BaseClass as argument with and without const
+			only access #2 is going to call and in that case (Linux Mint 21 64 bit) no warning
+			or error appeared on compile time.
+
+			Usually, void access(BaseClass &bc) and void access(const BaseClass &bc)
+			might be identical and shall raise an error on compile time.
+		*/
+
+		void access(const BaseClass &bc) {
+			/*
+				with the argument you're also able to do the same thing;
+				by using const this allows you only to read the members
+			*/
+
+			cout	<< "access #3:"
+					<< endl << "let's see the private member: " << bc.private_member << endl
+					<< "What about the protected member? " << bc.protected_member << endl;
+
+			// bc.unable_to_display();
+		}
+};
+
+int main() {
+	BaseClass bc;
+	ForeignClass fc;
+	fc.access();
+	fc.access(bc);
+
+	return 0;
+}
